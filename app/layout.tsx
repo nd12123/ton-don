a0baee30d34dd9f1e53b817928842acac219b201
TheWorldIsMine(andrey) <SemenovAA21@st.ithub.ru>
2025-05-20 21:12:25 +0300

    Design 5, deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+//import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {Toaster} from "react-hot-toast"
 //import Footer from "@/components/Footer";
@@ -9,8 +9,8 @@ import ThemeProviderWrapper from "@/components/ThemeProviderWrapper";
 //import Footer from "@/components/Footer"
 
 
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+//const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+//const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "TON Stake",
